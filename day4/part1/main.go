54a42ad89070af7a708494a3e70325cd4351369e
Julie Engel <julie.engel@mail.com>
2024-12-09 13:29:18 +0100

    solve day4 part1

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -2,78 +2,67 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"os"
 )
 
 func main() {
-	f, err := os.Open("day4/test.txt")
-	//f, err := os.Open("day4/input.txt")
-
+	//mainFile, err := os.Open("day4/test.txt")
+	//peekFile, err := os.Open("day4/test.txt")
+	mainFile, err := os.Open("day4/input.txt")
+	peekFile, err := os.Open("day4/input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer f.Close()
+	defer mainFile.Close()
+	defer peekFile.Close()
+
+	mainReader := bufio.NewReader(mainFile)
+	peekReader := bufio.NewReader(peekFile)
 
 	var found int
-	var unreadBuffer bytes.Buffer
-
 	var linesToCheck []string
 
-	r := bufio.NewReader(f)
 	for {
-		var currentLine string
-
-		// Read from the buffer first if there are unread lines
-		if unreadBuffer.Len() > 0 {
-			currentLine, err = unreadBuffer.ReadString('\n')
+		currentLine, err := mainReader.ReadString('\n')
+		if linesToCheck != nil {
+			peekLine, _ := peekReader.ReadString('\n')
+			// pop the first element
+			linesToCheck = linesToCheck[1:]
+			// add the next line
+			linesToCheck = append(linesToCheck, peekLine)
 		} else {
-			currentLine, err = r.ReadString('\n')
-		}
-
-		for pos, cell := range currentLine {
-			if cell == 'X' {
-				//fmt.Println("Found an X")
-				linesToCheck = nil
-
-				if checkLeftToRight(pos, currentLine) {
-					fmt.Println("Found XMAS while checking Left to Right")
-					found++
+			// initialise with the current line and the next 3 lines
+			for i := 0; i < 4; i++ {
+				peekLine, readErr := peekReader.ReadString('\n')
+				if readErr != nil {
+					break
 				}
-
-				if checkRightToLeft(pos, currentLine) {
-					fmt.Println("Found XMAS while checking Right to Left")
-					found++
-				}
-
-				linesToCheck = append(linesToCheck, currentLine)
-				for i := 0; i < 3; i++ {
-					followingLine, readErr := r.ReadString('\n')
-					linesToCheck = append(linesToCheck, followingLine)
-
-					// Write the peeked lines into the unreadBuffer
-					unreadBuffer.WriteString(followingLine)
-
-					if readErr != nil {
-						break
-					}
-				}
-
-				if checkTopToBottom(pos, linesToCheck) {
-					fmt.Println("Found XMAS while checking Top to Bottom")
-					found++
-				}
-
-				//checkBottomToTop()
-				//checkTopLeftToBottomRight()
-				//checkBottomRightToTopLeft()
-				//checkTopRightToBottomLeft()
-				//checkBottomLeftToTopRight()
+				linesToCheck = append(linesToCheck, peekLine)
 			}
 		}
 
+		for pos, cell := range currentLine {
+			if cell == 'X' || cell == 'S' {
+				if checkHorizontally(pos, currentLine) {
+					//fmt.Println("Found XMAS while checking Horizontally starting from X")
+					found++
+				}
+				if checkVertically(pos, linesToCheck) {
+					//fmt.Println("Found XMAS while checking Vertically starting from X")
+					found++
+				}
+				if checkDiagonallyOne(pos, linesToCheck) {
+					//fmt.Println("Found XMAS while checking Diagonally starting from X")
+					found++
+				}
+				if checkDiagonallyTwo(pos, linesToCheck) {
+					//fmt.Println("Found XMAS while checking Diagonally starting from X")
+					found++
+				}
+			}
+		}
 		if err != nil {
 			break
 		}
@@ -81,38 +70,21 @@ func main() {
 	fmt.Println("Found", found, "XMAS")
 }
 
-func checkLeftToRight(pos int, line string) bool {
-
+func checkHorizontally(pos int, line string) bool {
 	if pos+4 > len(line) {
 		//fmt.Println("Not enough characters left")
 		return false
 	}
 
 	// check if substring from pos to end of line contains XMAS
-	if line[pos:(pos+4)] == "XMAS" {
+	if line[pos:(pos+4)] == "XMAS" || line[pos:(pos+4)] == "SAMX" {
 		return true
 	}
 
 	return false
 }
 
-func checkRightToLeft(pos int, line string) bool {
-
-	if pos-4 < 0 {
-		return false
-	}
-
-	// check if substring from pos to start of line contains XMAS
-	substr := line[pos-3 : pos+1]
-	if substr == "SAMX" {
-		return true
-	}
-
-	return false
-}
-
-func checkTopToBottom(pos int, followingLines []string) bool {
-
+func checkVertically(pos int, followingLines []string) bool {
 	if len(followingLines) != 4 {
 		//fmt.Println("Not enough lines to check")
 		return false
@@ -122,11 +94,67 @@ func checkTopToBottom(pos int, followingLines []string) bool {
 
 	// iterate from line[pos] in the same column in the following lines to check if they contain XMAS
 	for i := 0; i < 4; i++ {
-		fmt.Println("Checking", string(followingLines[i][pos]))
+		if followingLines[i] == "" {
+			return false
+		}
 		possibleXmas += string(followingLines[i][pos])
 	}
 
-	if possibleXmas == "XMAS" {
+	if possibleXmas == "XMAS" || possibleXmas == "SAMX" {
+		return true
+	}
+
+	return false
+}
+
+func checkDiagonallyOne(pos int, followingLines []string) bool {
+	if len(followingLines) != 4 {
+		//fmt.Println("Not enough lines to check")
+		return false
+	}
+
+	var possibleXmas string
+
+	//checkTopLeftToBottomRight()
+	//checkBottomRightToTopLeft()
+	for i := 0; i < 4; i++ {
+		if followingLines[i] == "" {
+			return false
+		}
+		if pos+i >= len(followingLines[i]) {
+			return false
+		}
+		possibleXmas += string(followingLines[i][pos+i])
+	}
+
+	if possibleXmas == "XMAS" || possibleXmas == "SAMX" {
+		return true
+	}
+
+	return false
+}
+
+func checkDiagonallyTwo(pos int, followingLines []string) bool {
+	if len(followingLines) != 4 {
+		//fmt.Println("Not enough lines to check")
+		return false
+	}
+
+	var possibleXmas string
+
+	//checkTopRightToBottomLeft()
+	//checkBottomLeftToTopRight()
+	for i := 0; i < 4; i++ {
+		if followingLines[i] == "" {
+			return false
+		}
+		if pos-i < 0 {
+			return false
+		}
+		possibleXmas += string(followingLines[i][pos-i])
+	}
+
+	if possibleXmas == "XMAS" || possibleXmas == "SAMX" {
 		return true
 	}
 
